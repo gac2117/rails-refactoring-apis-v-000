50582d92babdf55a51c264838286d817fb3faf97
Grace Choi <oiograceoio@gmail.com>
2019-06-12 21:56:18 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,4 +1,5 @@
 class RepositoriesController < ApplicationController
+
   def index
   	github = GithubService.new
   	@repos_array = github.get_repos
@@ -7,6 +8,6 @@ class RepositoriesController < ApplicationController
   def create
     github = GithubService.new
     github.create_repo(params[:name])
-    redirect_to '/'
+    redirect_to root_path
   end
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,16 +1,13 @@
 class GithubService
 	attr_reader :access_token
+	require 'pry'
 
 	def initialize(access_hash = nil)
 		@access_token = access_hash["access_token"] if access_hash
 	end
 
 	def authenticate!(client_id, client_secret, code)
-		response = Faraday.post "https://github.com/login/oauth/access_token", {
-			client_id: client_id,
-			client_secret: client_secret,
-			code: code
-		}, {'Accept' => 'application/json'}
+		response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client_id, client_secret: client_secret,code: code}, {'Accept' => 'application/json'}
 		access_hash = JSON.parse(response.body)
 		@access_token = access_hash["access_token"]
 	end	
@@ -22,12 +19,12 @@ class GithubService
 	end
 
 	def get_repos
-		response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+		response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}"}
 	    repos_array = JSON.parse(response.body)
 	    repos_array.map{|repo| GithubRepo.new(repo)}
 	end
 
 	def create_repo(name)
-		Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+		Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}"}
 	end
 end
